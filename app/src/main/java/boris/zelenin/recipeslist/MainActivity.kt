2d139732cddcaa83d653fecc560ccb5bf807b7f1
ba.zelenin <ba.zelenin@tensor.ru>
2019-11-10 14:28:44 +0000

    Initial commit

---
+++ app/src/main/java/boris/zelenin/recipeslist/MainActivity.kt
@@ -0,0 +1,19 @@
+package boris.zelenin.recipeslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import boris.zelenin.recipeslist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/boris/zelenin/recipeslist/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package boris.zelenin.recipeslist.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
